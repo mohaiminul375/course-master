d26907c763c5c74b19fe4befe576743856b954b7
Mohaiminul375 <mohaiminul375@gmail.com>
2025-12-01 21:09:58 +0600

    create layout register login

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Shared/Navbar";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from 'react-hot-toast';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -39,8 +40,7 @@ export default function RootLayout({
             {children}
           </main>
         </ThemeProvider>
-
-
+        <Toaster />
       </body>
     </html>
   );
